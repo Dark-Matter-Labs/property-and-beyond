3eb4d1ca74f29e9e7cbebac635cb0e58715d1099
theocampbell <57679708+theocampbell@users.noreply.github.com>
2024-02-02 15:06:41 +0100

    fix nav

--- components/navbar.js
+++ components/navbar.js
@@ -6,20 +6,16 @@ export default function NavBar() {
   const pathname = usePathname();
   if (pathname !== '/studio/structure') {
     return (
-      <div className='text-white bg-black h-20 flex flex-row justify-between'>
-        {' '}
-        <Link href='/' className='h-full'>
+      <div className='text-white bg-black h-20 flex items-center justify-center'>
+        <div className='global-margin flex flex-row justify-between items-center h-full w-full'>
+ 
+        <Link href='/' className=''>
           NAVBAR
         </Link>
-        <Link href='/map' className='h-full'>
+        <Link href='/map' className=''>
           MAP
-        </Link>{' '}
-        <Link
-          href='/map/3clim-cyberwarfare-and-digital-weapons-of-mass-destabilization'
-          className='h-full'
-        >
-          riskpage
         </Link>
+        </div>
       </div>
     );
   }
